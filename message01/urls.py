b1228d73b157a31e396aba3d03616f07a6fbc1a2
Ma1sadaze <mar1sadaze@qq.com>
2018-04-18 14:16:10 +0800

    新建在线课程项目

---
+++ message01/urls.py
@@ -0,0 +1,29 @@
+"""message01 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+import sys
+import  os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+]
